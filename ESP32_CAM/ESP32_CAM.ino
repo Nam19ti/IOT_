aa93d4aa968259202008c1e42f7bff508bba93d2
talkwitht21-stack <talkwitht21@gmail.com>
2026-08-16 14:56:18 +0700

    Fix IOT_2

--- ESP32_CAM/ESP32_CAM.ino
+++ ESP32_CAM/ESP32_CAM.ino
@@ -6,8 +6,8 @@ const char* WIFI_SSID     = "NONNET";
 const char* WIFI_PASSWORD = "abcd1234";
 
 // Cấu hình IP Tĩnh: 192.168.137.233
-IPAddress local_IP(192.168, 137, 233);
-IPAddress gateway(192.168, 137, 1);
+IPAddress local_IP(192, 168, 137, 233);
+IPAddress gateway(192, 168, 137, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(8, 8, 8, 8);
 
--- IOT_2/IOT_2.ino
+++ IOT_2/IOT_2.ino
@@ -18,8 +18,8 @@ const int   PYTHON_SERVER_PORT = 5000;
 // =========================================================
 // CẤU HÌNH IP TĨNH CHO IOT_2 (ESP32)
 // =========================================================
-IPAddress local_IP(192.168, 137, 199);
-IPAddress gateway(192.168, 137, 1);
+IPAddress local_IP(192, 168, 137, 199);
+IPAddress gateway(192, 168, 137, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(8, 8, 8, 8);
 
